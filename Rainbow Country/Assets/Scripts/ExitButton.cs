0b8e5002c29bf6c92a967bce856baa837c0a2625
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-10-21 15:08:03 -0700

    added menu

---
+++ Rainbow Country/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Application.Quit();
+    }
+
+    private void OnMouseEnter()
+    {
+        gameObject.GetComponent<TextMesh>().color = Color.magenta;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<TextMesh>().color = Color.white;
+
+    }
+}
