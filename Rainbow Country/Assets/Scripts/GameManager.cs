4d31a44aaa4956a8ca471a132b4988b183e8710c
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-10-22 20:02:17 -0700

    class work

--- Rainbow Country/Assets/Scripts/GameManager.cs
+++ Rainbow Country/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
@@ -9,13 +10,20 @@ public class GameManager : MonoBehaviour {
     public GameObject timerBackground;
     private float timer;
     private float timer2;
+
     private bool hasSetColors = false;
+    private bool hasWon = false;
 
     public GameObject camera1;
     public GameObject camera2;
 
     public GameObject player1;
     public GameObject player2;
+
+    public GameObject winText;
+
+    private float endTime;
+    private bool hasStartedEndTime = false;
 
 
 	// Use this for initialization
@@ -47,9 +55,14 @@ public class GameManager : MonoBehaviour {
             if (!hasSetColors)
             {
                 Color color1 = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
+                Debug.Log("R: " + color1.r + " G: " + color1.g + " B: " + color1.b);
+                float h, s, v;
+                Color.RGBToHSV(color1, out h, out s, out v);
+                Debug.Log("HUE : " + h);
+
                 Color color2 = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
 
-                while (Mathf.Abs(color1.r - color2.r) >= 0.5 && Mathf.Abs(color1.g - color2.g) >= 0.5 && Mathf.Abs(color1.b - color2.b) >= 0.5)
+                while (Mathf.Abs(color1.r - color2.r) <= 0.5 && Mathf.Abs(color1.g - color2.g) <= 0.5 && Mathf.Abs(color1.b - color2.b) <= 0.5)
                 {
                     color2 = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
                 }
@@ -64,6 +77,30 @@ public class GameManager : MonoBehaviour {
         {
             timerText.SetActive(false);
             timerBackground.SetActive(false);
+        }
+
+        if (hasSetColors && Input.GetKeyDown(KeyCode.E) && !hasWon)
+        {
+            winText.GetComponent<Text>().text = "PLAYER 1 WINS";
+            winText.SetActive(true);
+            hasWon = true;
+
+            endTime = Time.time + 3f;
+            hasStartedEndTime = true;
+        }
+        else if (hasSetColors && Input.GetKeyDown(KeyCode.Return) && !hasWon)
+        {
+            winText.GetComponent<Text>().text = "PLAYER 2 WINS";
+            winText.SetActive(true);
+            hasWon = true;
+
+            endTime = Time.time + 3f;
+            hasStartedEndTime = true;
+        }
+
+        if (hasStartedEndTime && endTime <= Time.time)
+        {
+            SceneManager.LoadScene(0);
         }
 	}
 }
--- Rainbow Country/Assets/Scripts/MenuManager.cs
+++ Rainbow Country/Assets/Scripts/MenuManager.cs
@@ -7,24 +7,19 @@ public class MenuManager : MonoBehaviour {
 
     public GameObject titleText;
     public GameObject camera1;
-
-    private float nextActionTime = 0.0f;
-    private float period = 1f;
+    
 
     // Use this for initialization
     void Start () {
-		
-	}
+
+        Color c = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
+        titleText.GetComponent<Text>().color = c;
+        Color d = new Color(1.0f - c.r, 1.0f - c.g, 1.0f - c.b);
+        camera1.GetComponent<Camera>().backgroundColor = d;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.time > nextActionTime)
-        {
-            nextActionTime += period;
-            Color c = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
-            titleText.GetComponent<Text>().color = c;
-            Color d = new Color(1.0f - c.r, 1.0f - c.g, 1.0f - c.b);
-            camera1.GetComponent<Camera>().backgroundColor = d;
-        }
+
     }
 }
